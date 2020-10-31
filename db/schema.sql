78cb9e78d3208f47f62af394341acd6d120313ed
Plain Jane <jane99hsu@gmail.com>
2020-10-30 21:00:20 -0700

    added all columns for models

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,6 @@
+-- With Sequelize, we no longer have to create the entire SQL table schema
+-- we just create a database, start app, and Sequelize will create the tables
+
 -- DROP DATABASE
 DROP DATABASE IF EXISTS ecommerce_db;
 
