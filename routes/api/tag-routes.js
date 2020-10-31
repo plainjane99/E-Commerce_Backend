d39739de7bb669aab9260638d39ddf8d5ec0b28b
Plain Jane <jane99hsu@gmail.com>
2020-10-31 14:53:42 -0700

    added delete routes for category, product, tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -72,6 +72,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbTag => {
+    if(!dbTag) {
+      res.status(404).json({ message: 'No category found with this id'});
+      return;
+    }
+    res.json(dbTag);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
